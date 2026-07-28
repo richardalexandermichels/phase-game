e030543f97b63bc2bc3a2264b0233ab65ca6d98c
Richard Michels <richard.alexandermichels@gmail.com>
2026-07-27 20:08:07 -0400

    added rhythm clock tests

--- app/src/test/java/com/rmichels/phasegame/ExampleUnitTest.kt
+++ app/src/test/java/com/rmichels/phasegame/ExampleUnitTest.kt
@@ -24,32 +24,6 @@ class ExampleUnitTest {
     }
 
     @Test
-    fun rhythmClock_derivesBarsAndPhasesFromOneOrigin() {
-        val clock = RhythmClock(stepDurationMs = 250L, stepsPerBar = 12)
-        clock.start(nowMs = 1_000L)
-
-        assertEquals(0L, clock.absoluteBarIndex(nowMs = 3_999L))
-        assertEquals(1L, clock.absoluteBarIndex(nowMs = 4_000L))
-        assertEquals(0, clock.phaseIndexForBar(3L, repetitionsPerPhase = 4L))
-        assertEquals(1, clock.phaseIndexForBar(4L, repetitionsPerPhase = 4L))
-        assertEquals(0, clock.phaseIndexForBar(48L, repetitionsPerPhase = 4L))
-    }
-
-    @Test
-    fun rhythmClock_pauseFreezesProgressAndResumePreservesPosition() {
-        val clock = RhythmClock(stepDurationMs = 250L, stepsPerBar = 4)
-        clock.start(nowMs = 1_000L)
-
-        clock.pause(nowMs = 1_375L)
-        assertEquals(375L, clock.elapsedMs(nowMs = 9_000L))
-        assertEquals(0L, clock.absoluteBarIndex(nowMs = 9_000L))
-
-        clock.resume(nowMs = 2_375L)
-        assertEquals(375L, clock.elapsedMs(nowMs = 2_375L))
-        assertEquals(625L, clock.elapsedMs(nowMs = 2_625L))
-    }
-
-    @Test
     fun nearestHit_usesThePatternAssignedToEachAdjacentBar() {
         val clock = RhythmClock(stepDurationMs = 250L, stepsPerBar = 4)
         clock.start(nowMs = 1_000L)
